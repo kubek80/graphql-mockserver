f52238bafbe7cc37ce6c0d8c60908787dd00d641
kubek80 <jakub.zwirko@gmail.com>
2017-06-18 23:15:46 +1000

    Added mock schema

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import schema from './schema';
+
+const app = express();
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(3500);
